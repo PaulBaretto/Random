3d0e6e03fc9ae59c1f9b13143ae4727341731d82
Paul Barretto <paulbaretto827@gmail.com>
2023-11-24 14:20:33 +0530

    update 01

---
+++ masonary-grid/template-4/assets/js/main.js
@@ -0,0 +1,17 @@
+
+
+// var elem = document.querySelector('.ms-wrapper');
+// var msnry = new Masonry(grid, {
+//     // options
+//     // itemSelector: '.grid-item',
+//     // columnWidth: 200
+// });
+
+
+// window.onload = () => {
+//     let grid = document.querySelector('.ms-wrapper');
+//     let newMs = new men Masonry(gird)
+// }
+
+
+for
