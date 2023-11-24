6fce7e299a77f4fb75e5355a5d2ef4a119d1f34f
Paul Barretto <paulbaretto827@gmail.com>
2023-11-24 16:41:25 +0530

    test-panding

--- masonary-grid/template-4/assets/js/main.js
+++ masonary-grid/template-4/assets/js/main.js
@@ -1,17 +1,7 @@
 
 
-// var elem = document.querySelector('.ms-wrapper');
-// var msnry = new Masonry(grid, {
-//     // options
-//     // itemSelector: '.grid-item',
-//     // columnWidth: 200
-// });
+let div = document.querySelectorAll('.masonry-details');
 
-
-// window.onload = () => {
-//     let grid = document.querySelector('.ms-wrapper');
-//     let newMs = new men Masonry(gird)
-// }
-
-
-for
+    div.forEach(divs => {
+        divs.remove();
+    });
