30ccee81116ec038ec9bee575a544e491d69bbdf
Paul Barretto <paulbaretto827@gmail.com>
2023-12-01 19:13:05 +0530

    Working on faq

--- nav-bar/template-1/assets/js/main.js
+++ nav-bar/template-1/assets/js/main.js
@@ -10,6 +10,7 @@ let open_mega = document.querySelector('.nb-meganav-mob')
 let mob_mega = document.getElementById("sm-mega")
 let open_lists = document.querySelectorAll('.nbmeg-list-mob')
 let mob_list = document.getElementById("sm-list")
+let sm_nav_show_btn = document.querySelector('.nb-toggler');
 
 
 mob_sub.addEventListener('click', () => {
@@ -28,3 +29,15 @@ open_lists.forEach(open_list => {
 });
 
 
+sm_nav_show_btn.addEventListener('click', () => {
+    smNav.classList.toggle('sm-show-nav-items')
+    let navWrap = document.querySelector('.nav-wrap');
+    // console.log(navWrap)
+    if (smNav.classList.contains('sm-show-nav-items')){
+        sm_nav_show_btn.textContent = 'Close';
+        navWrap.style.cssText = 'border-radius:20px 20px 0px 0px';
+    }else{
+        sm_nav_show_btn.textContent = 'Menu';
+        navWrap.style.cssText = 'border-radius:20px 20px 20px 20px';
+    }
+})
